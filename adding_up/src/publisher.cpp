09d4e0bfd0558bf8ca30484e818119253fabb600
Yael Bakker <me@yael.fyi>
2021-02-08 23:32:38 +0100

    add adding_up assigment

--- adding_up/src/publisher.cpp
+++ adding_up/src/publisher.cpp
@@ -22,27 +22,27 @@ class MinimalPublisher : public rclcpp::Node
 {
 public:
   MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
+      : Node("minimal_publisher"), count_(0)
   {
-    publisher_ = this->create_publisher<tutorial_interfaces::msg::Num>("topic", 10);    
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    publisher_ = this->create_publisher<tutorial_interfaces::msg::Num>("topic", 10);
+    //write message every 2s
+    timer_ = this->create_wall_timer(2000ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
 
 private:
   void timer_callback()
   {
-    auto message = tutorial_interfaces::msg::Num();                               
-    message.num = this->count_++;                                        
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.num);    
+    auto message = tutorial_interfaces::msg::Num();
+    message.num = this->count_++;
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.num);
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<tutorial_interfaces::msg::Num>::SharedPtr publisher_;         
+  rclcpp::Publisher<tutorial_interfaces::msg::Num>::SharedPtr publisher_;
   size_t count_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
--- adding_up/src/subscriber.cpp
+++ adding_up/src/subscriber.cpp
@@ -11,28 +11,46 @@
 #include <memory>
 
 #include <rclcpp/rclcpp.hpp>
-#include "tutorial_interfaces/msg/num.hpp"     
+#include "tutorial_interfaces/msg/num.hpp"
 using std::placeholders::_1;
 
 class MinimalSubscriber : public rclcpp::Node
 {
 public:
   MinimalSubscriber()
-  : Node("minimal_subscriber")
+      : Node("minimal_subscriber")
   {
-    subscription_ = this->create_subscription<tutorial_interfaces::msg::Num>(          
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    subscription_ = this->create_subscription<tutorial_interfaces::msg::Num>(
+        "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
 private:
-  void topic_callback(const tutorial_interfaces::msg::Num::SharedPtr msg) const       
+  int count = 0;
+  int nums[3];
+  void handleNumber(int num)
   {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->num);              
+    if (count < 3)
+    {
+      nums[count] = num;
+      count++;
+    }
+    else
+    {
+      count = 0;
+      int sum = 0;
+      for (auto &num : nums)
+        sum += num;
+      RCLCPP_INFO(this->get_logger(), "Sum: '%d'", sum);
+    }
   }
-  rclcpp::Subscription<tutorial_interfaces::msg::Num>::SharedPtr subscription_;       
+  void topic_callback(const tutorial_interfaces::msg::Num::SharedPtr msg)
+  {
+    handleNumber(msg->num);
+  }
+  rclcpp::Subscription<tutorial_interfaces::msg::Num>::SharedPtr subscription_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
